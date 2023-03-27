1982a07ae42e96ca62f1be3c55291ed0d9cf520d
jjeongdong <donghun3296@naver.com>
2023-03-27 14:23:01 +0900

    chore : application.properties 암호화

---
+++ src/test/java/com/example/testproject/config/JasyptTest.java
@@ -0,0 +1,29 @@
+package com.example.testproject.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JasyptTest {
+
+    @Test
+    void encryptTest(){
+        String id = "ddd";
+        String password = "ddd";
+
+        System.out.println(jasyptEncoding(id));
+        System.out.println(jasyptEncoding(password));
+    }
+
+    public String jasyptEncoding(String value) {
+        String key = "oehdgns1892";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
+    }
+
+}
